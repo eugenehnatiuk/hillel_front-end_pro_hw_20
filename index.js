9d075c972dfce50a740a6585519089d628e9f1dd
Eugene Hnatiuk <gnatyuk.e@gmail.com>
2024-01-29 17:13:28 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const balances = [
+  '$123.10',
+  '$61 123.23',
+  '$1,321,123.10',
+  '$0.00',
+  '$799.50',
+  '$1 916 161 123.25'
+];
+
+const balanceNums = balances.map((balance) => Number(balance.match(/[\d\.]/g).join('')));
+  
+console.log(balanceNums)
+
