7d95ee7f4edabc0aec3b39f5f0e6804d733eb439
Eugene Hnatiuk <gnatyuk.e@gmail.com>
2024-02-01 12:41:34 +0200

    roll over last commit with the next hw

--- index.js
+++ index.js
@@ -1,31 +1,14 @@
-/* '   123456  ' -> true
-' -0123456  ' -> true
-'     -12345  ' -> false
-'   -023456' -> false
- '-0234560' -> true,
-' -000016   ' -> false
-' -000000123456  ' -> true
-' - 000000123456  ' -> false
-' +000123456  ' -> true 
- '100000' -> true*/
-
-const sixDigitsArr = [
-  '   123456  ',
-  ' -0123456  ',
-  '     -12345  ',
-  '   -023456',
-  '-0234560',
-  ' -000016   ',
-  ' -000000123456  ',
-  ' - 000000123456  ',
-  ' +000123456  ',
-  '100000'
+const balances = [
+  '$123.10',
+  '$61 123.23',
+  '$1,321,123.10',
+  '$0.00',
+  '$799.50',
+  '$1 916 161 123.25',
 ];
 
-const sixDigitsRegEx = /^\s*[+-]?0*[1-9]\d{5}\s*$/;
+const balanceNums = balances.map((balance) =>
+  Number(balance.match(/[\d\.]/g).join(''))
+);
 
-const sixDigits = sixDigitsArr.map((element) => `${element.trim()} -> ${sixDigitsRegEx.test(element)}`);
-
-console.log(sixDigits);
-
-
+console.log(balanceNums);
